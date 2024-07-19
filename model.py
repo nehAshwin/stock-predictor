b4347fe59318338fffa6a6a922b020a482fa17fb
nehAshwin <nashwin2548@gmail.com>
2024-07-18 22:44:32 -0700

    plz work

---
+++ model.py
@@ -0,0 +1,96 @@
+from web_scrape import *
+import numpy as np
+import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from keras import Sequential 
+from keras import layers
+
+# load data
+df = scrape_yahoo_finance('GOOG')
+
+# Preprocess data
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+df['Adj Close'] = pd.to_numeric(df['Adj Close'], errors='coerce')
+
+# Simple Moving Average per 20 days
+df['Moving Avg'] = df['Adj Close'].rolling(20).mean()
+df.dropna(inplace=True)
+
+# Percent Change
+df['Percent Change'] = df['Adj Close'].pct_change()
+
+# print(df[['Adj Close', 'Percent Change']].head())
+
+# Preprocessing Data
+adj_close_price = df.loc[:, ['Adj Close']]
+# print(max(adj_close_price.values), min(adj_close_price.values))
+# print(type(adj_close_price))
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(adj_close_price)
+# print(scaled_data)
+
+# Split Data
+
+x_data = []
+y_data = []
+
+for i in range(100, len(scaled_data)):
+    x_data.append(scaled_data[i-100:i])
+    y_data.append(scaled_data[i])
+
+x_data, y_data = np.array(x_data), np.array(y_data)
+
+splitting_len = int(len(x_data) * 0.7)
+x_train = x_data[:splitting_len]
+y_train = y_data[:splitting_len]
+
+x_test = x_data[splitting_len:]
+y_test = y_data[splitting_len:]
+
+# print(x_train.shape)
+# print(y_train.shape)
+# print(x_test.shape)
+# print(y_test.shape)
+
+# Building Model (Neural Network)
+
+model = Sequential()
+model.add(layers.LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(layers.LSTM(64, return_sequences=False))
+model.add(layers.Dense(25))
+model.add(layers.Dense(1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit(x_train, y_train, batch_size=1, epochs=2)
+
+# model.summary()
+
+# Predicting Test Data
+
+predictions = model.predict(x_test)
+
+inv_predictions = scaler.inverse_transform(predictions)
+# print(inv_predictions)
+inv_y_test = scaler.inverse_transform(y_test)
+# print(inv_y_test)
+
+rmse = np.sqrt(np.mean((inv_predictions - inv_y_test)**2))
+# print(rmse)
+
+plotting_data = pd.DataFrame(
+    {
+        'original_test_data': inv_y_test.reshape(-1), 'predictions': inv_predictions.reshape(-1)
+    }, index = df.index[splitting_len +100:]
+)
+
+# print(plotting_data.head)
+
+plot_graph((15,6), pd.concat([adj_close_price[:splitting_len+100], plotting_data], axis=0), 'whole data')
+
+model.save("Latest_stock_price_model.keras")
---
+++ web_scrape.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def scrape_yahoo_finance(ticker):
+    # goal for later: have the user input the current date and pull 5 years from there instead of fixed 5 years (check gpt)
+    url = f'https://finance.yahoo.com/quote/{ticker}/history/?period1=1561659807&period2=1719511052'
+    # Add headers to mimic a browser request
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    #HTTP GET request to finance.yahoo
+    #headers is necessary to make client recognizable by server
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.find('table')
+
+        if table:
+            rows = table.find_all('tr')[1:]  # Skip the header row
+            data = []
+
+            for row in rows:
+                cols = row.find_all('td')
+
+                if len(cols) < 6:
+                    continue
+
+                date = cols[0].text
+                adj_close_price = cols[5].text.replace(',', '')
+                data.insert(0, [date, adj_close_price])
+            
+            df = pd.DataFrame(data, columns=['Date', 'Adj Close'])
+            # df['MA'] = df['Close'].rolling(5).mean()
+
+            return df
+        
+        else:
+            print(f"No data found for {ticker}")
+            return pd.DataFrame(columns=['Date', 'Adj Close'])
+        
+    else:
+        print(f"Failed to retrieve data for {ticker}")
+        return pd.DataFrame(columns=['Date', 'Adj Close'])
+    
+def plot_graph(figsize, values, column_name):
+    plt.figure(figsize = figsize)
+    values.plot()
+    plt.xlabel("years")
+    plt.ylabel(column_name)
+    plt.title(f"{column_name} of Google data")
+    plt.show() 
+
+# print(scrape_yahoo_finance('GOOG'))
+# plot_graph((15,5), scrape_yahoo_finance('GOOG')['Close'], 'Closing Price')
